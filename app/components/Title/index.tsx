6da628279dceeb0d65892b260097396829e8e80b
h-yabi2 <hur4k0dt@gmail.com>
2024-05-20 19:06:38 +0900

    Add color prop to Title component

--- app/components/Title/index.tsx
+++ app/components/Title/index.tsx
@@ -5,11 +5,16 @@ const s = styles
 interface TitleProps {
   title: string
   subTitle: string
+  color?: ['default' | 'white']
 }
 
-export const Title: React.FC<TitleProps> = ({ title, subTitle }) => {
+export const Title: React.FC<TitleProps> = ({
+  title,
+  subTitle,
+  color = 'default',
+}) => {
   return (
-    <h2 className={s.root}>
+    <h2 className={`${s.root} ${color === 'white' ? s.white : ''}`}>
       {title}
       <span>{subTitle}</span>
     </h2>
