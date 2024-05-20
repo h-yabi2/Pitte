4b8a5a9c4c0c09aeb97a76fe0ce9653401d3636f
h-yabi2 <hur4k0dt@gmail.com>
2024-05-20 14:49:33 +0900

    Add logo to the header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Menu } from '@/app/components/Menu'
+
+export const Header: React.FC = () => {
+  return (
+        <header className='flex justify-between items-center py-4 px-5 relative'>
+            <Link href="/">
+                <Image src="/images/logo.svg" alt="Pitte" width={160} height={58} className='' />
+            </Link>
+            <Menu />
+        </header>
+  )
+}
---
+++ app/components/MenuItem.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import Link from 'next/link'
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
+
+export const MenuItem = ({ i }: { i: number }) => {
+//   const style = { border: `2px solid ${colors[i]}` };
+  return (
+    <div>
+        <motion.li
+        variants={variants}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        >
+            <Link href="#feature">Pitteの特徴</Link>
+        </motion.li>
+        <motion.li
+        variants={variants}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        >
+            <Link href="#price">料金</Link>
+        </motion.li>        
+        <motion.li
+        variants={variants}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        >
+            <Link href="#about">こんな方におすすめ</Link>
+        </motion.li>
+        <motion.li
+        variants={variants}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        >
+            <Link href="#about">選ばれる4つの理由</Link>
+        </motion.li>                
+    </div>    
+  );
+};
