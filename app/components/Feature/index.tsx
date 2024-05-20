5fbb58c9b0b6e7e67727a8e402155900e9e66fe6
h-yabi2 <hur4k0dt@gmail.com>
2024-05-20 19:56:30 +0900

    Add Feature component with styles module for design

---
+++ app/components/Feature/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles.module.scss'
+const s = styles
+
+export const Feature: React.FC = () => {
+  return (
+    <ul>
+      <li>asetea</li>
+    </ul>
+  )
+}
