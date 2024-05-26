660851d2e499e9a32b3e32043e751e93af19c170
h-yabi2 <hur4k0dt@gmail.com>
2024-05-26 22:33:25 +0900

    Add animation to highlighted text in Contents component

--- app/components/Contents/index.tsx
+++ app/components/Contents/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import styles from './styles.module.scss'
 const s = styles
@@ -5,17 +7,67 @@ const s = styles
 import { Feature } from '@/app/components/Feature'
 import { Price } from '@/app/components/Price'
 import { Recommended } from '@/app/components/Recommended'
+import { motion, Variants } from 'framer-motion'
+
+const animationProps = {
+  style: {
+    backgroundImage: 'linear-gradient(transparent, 70%, yellow 70%)',
+    backgroundRepeat: 'no-repeat',
+    transition: 'background-size 0.5s',
+  },
+  viewport: {
+    once: true,
+  },
+}
+
+const itemVariants = (): Variants => {
+  return {
+    offscreen: {
+      backgroundSize: '0% 100%',
+    },
+    onscreen: {
+      backgroundSize: '100% 100%',
+      transition: {
+        type: 'spring',
+        duration: 0.3,
+        delay: 0.3,
+      },
+    },
+  }
+}
 
 export const Contents: React.FC = () => {
   return (
     <main>
       <h1 className={s.title}>
-        <span className="text-highlight">『東京・綾瀬駅』</span>
+        <motion.span
+          {...animationProps}
+          initial="offscreen"
+          whileInView="onscreen"
+          variants={itemVariants()}
+        >
+          『東京・綾瀬駅』
+        </motion.span>
         に完全無人の
         <br className="mediaSP" />
-        <span className="text-highlight">セルフ脱毛サロン</span>がOPEN！！
+        <motion.span
+          {...animationProps}
+          initial="offscreen"
+          whileInView="onscreen"
+          variants={itemVariants()}
+        >
+          セルフ脱毛サロン
+        </motion.span>
+        がOPEN！！
         <br />
-        <span className="text-highlight">業務用の高品質な脱毛器</span>
+        <motion.span
+          {...animationProps}
+          initial="offscreen"
+          whileInView="onscreen"
+          variants={itemVariants()}
+        >
+          業務用の高品質な脱毛器
+        </motion.span>
         が<br className="mediaSP" />
         格安で使い放題◎
       </h1>
