7b2fc500919ef5c7cceed6b342e575c6085abad6
h-yabi2 <hur4k0dt@gmail.com>
2024-05-26 12:23:22 +0900

    Update header style

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import styles from './styles.module.scss'
+const s = styles
 import Image from 'next/image'
 import Link from 'next/link'
 import { Menu } from '@/app/components/Menu'
 
 export const Header: React.FC = () => {
   return (
-    <header className='flex justify-between items-center py-4 px-5 relative'>
+    <header className="flex justify-between items-center py-3 md:py-4 px-4 md:px-5 relative">
       <Link href="/">
-          <Image src="/images/logo.svg" alt="Pitte" width={160} height={58} className='' />
+        <Image
+          src="/images/logo.svg"
+          alt="Pitte"
+          width={160}
+          height={58}
+          className={s.logo}
+        />
       </Link>
       <Menu />
     </header>
