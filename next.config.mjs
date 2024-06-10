36bced958fcafba4814e4b486b6614a832391da5
h-yabi2 <hur4k0dt@gmail.com>
2024-06-10 17:36:35 +0900

    Add image caching and headers configuration in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-      return config
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+  images: {},
+  headers: [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 's-maxage=1, stale-while-revalidate=59',
+        },
+      ],
     },
-    images: {
-    },
-  }
-  
-  export default nextConfig
-  
+  ],
+}
+
+export default nextConfig
