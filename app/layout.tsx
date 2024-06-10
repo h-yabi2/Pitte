ddad858ee077e84da3f4a4c8a5540672162a8ea7
h-yabi2 <hur4k0dt@gmail.com>
2024-06-10 15:51:07 +0900

    robots, index（インデックス） 設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,14 @@ export const metadata: Metadata = {
   title: 'セルフ脱毛サロン Pitte',
   description:
     '『東京・綾瀬駅』に完全無人のセルフ脱毛サロンがOPEN！！業務用の高品質な脱毛器が格安で使い放題◎',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    noarchive: true,
+    noimageindex: true,
+    nosnippet: true,
+  },
 }
 
 export default function RootLayout({
